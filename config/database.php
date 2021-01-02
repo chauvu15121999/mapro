540a46f047af550389acc8e02fabc0e5ea1fdc36
chauvu <chauvu5044@gmail.com>
2021-01-02 23:26:15 +0700

    update

--- config/database.php
+++ config/database.php
@@ -1,4 +1,4 @@
-.<?php
+<?php
 
 use Illuminate\Support\Str;
 
@@ -92,16 +92,17 @@ return [
         ],
         'mongodb' => [
             'driver' => 'mongodb',
-            'dsn' => env('MONGO_DB_DSN'),
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '27017'),
             'database' => env('DB_DATABASE', 'mapro'),
-            // 'username' => env('DB_USERNAME', 'mapro'),
-            // 'password' => env('DB_PASSWORD', '0768415044chauvu'),
-            // 'options' => [
-            //     // here you can pass more settings to the Mongo Driver Manager
-            //     // see https://www.php.net/manual/en/mongodb-driver-manager.construct.php under "Uri Options" for a list of complete parameters that you can use
+            'username' => env('DB_USERNAME', ''),
+            'password' => env('DB_PASSWORD', ''),
+            'options' => [
+                // here you can pass more settings to the Mongo Driver Manager
+                // see https://www.php.net/manual/en/mongodb-driver-manager.construct.php under "Uri Options" for a list of complete parameters that you can use
 
-            //     'database' => env('DB_AUTHENTICATION_DATABASE', 'admin'), // required with Mongo 3+
-            // ],
+                'database' => env('DB_AUTHENTICATION_DATABASE', 'admin'), // required with Mongo 3+
+            ],
         ],
     ],
 
