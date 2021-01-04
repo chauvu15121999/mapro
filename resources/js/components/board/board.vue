53df9c87f3777df7978be73e961ed6530502d631
chauvu <chauvu5044@gmail.com>
2021-01-04 17:05:19 +0700

    update

--- resources/js/components/board/board.vue
+++ resources/js/components/board/board.vue
@@ -173,7 +173,7 @@
                         this.users =  this.Member.getMembers[i];
                     }
                 }
-                    if(this.users == null){
+                    if(this.users == ''){
                        window.history.go(-1);
                     }
             },
