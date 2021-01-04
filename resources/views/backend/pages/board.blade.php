9b6b8db9e5b8d178bf1e4befa4b182c2a542ae74
chauvu <chauvu5044@gmail.com>
2021-01-04 17:41:34 +0700

    update

--- resources/views/backend/pages/board.blade.php
+++ resources/views/backend/pages/board.blade.php
@@ -1,11 +1,15 @@
 @extends('backend.layout.index')
 @section('content')
 	<?php
-		print_r($board['members']); 
+	for($i = 0 ; $i < count($board['members']) ; $i++){
+		 if($board['members'][$i]['user_email'] != Auth::user()->email){
+		 	header("location:javascript://history.go(-1)");
+		 } 
+		}
 	?>
-{{-- 	<?php  $board =  json_encode($board); ?>
+	<?php  $board =  json_encode($board); ?>
 	<board
 	 :user="{{Auth()->user()}}"  
 	:board="{{$board}}"
-	/> --}}
+	/>
 @endsection 
